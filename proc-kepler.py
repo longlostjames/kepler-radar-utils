e307ed1c5cdb211d5b4bf55ec7ae01b81ed22eb5
longlostjames <chris.walden@ncas.ac.uk>
2023-02-15 09:55:59 +0000

    inpath edits

--- proc-kepler.py
+++ proc-kepler.py
@@ -1,13 +1,14 @@
 import getopt, sys, os
 import glob
 
+
 from datetime import datetime
 
-sys.path.append('/home/users/cjwalden/my-packages')
+#sys.path.append('/home/users/cjwalden/my-packages')
 import kepler_utils as kepler
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "d:p:c:", ["date=","path=","campaign="])
+    opts, args = getopt.getopt(sys.argv[1:], "d:i:c:o:", ["date=","inpath=","campaign=","outpath="])
 except getopt.GetoptError as err:
         # print help information and exit:
         print(err) # will print something like "option -a not recognized
@@ -15,25 +16,28 @@ except getopt.GetoptError as err:
 
 data_date = datetime.now()
 datestr = data_date.strftime('%Y%m%d')
-data_path = '/gws/pw/j07/ncas_obs_vol2/cao/raw_data/ncas-radar-mobile-ka-band-1/data/'
+inpath = '/gws/pw/j07/ncas_obs_vol2/cao/raw_data/ncas-radar-mobile-ka-band-1/data/'
+outpath = '/gws/pw/j07/ncas_obs_vol2/cao/processing/ncas-radar-mobile-ka-band-1/'
 
 
 for o, a in opts:
     if o == "-d":
         datestr = a;
-    elif o == "-p":
-        data_path = a
+    elif o == "-i":
+        inpath = a;
     elif o == "-c":
-        campaign = a.lower()
+        campaign = a.lower();
+    elif o == "-o":
+        outpath = a;
     else:
         assert False, "unhandled option"
 
 dateyr = datestr[0:4]
 
 if campaign == "long-term":
-    mmclxpath = os.path.join(data_path,"long-term","raw",dateyr,datestr);
+    mmclxpath = os.path.join(inpath,"long-term","mom",dateyr,datestr);
 else:
-    mmclxpath = os.path.join(data_path,"campaign",campaign,"raw",datestr);
+    mmclxpath = os.path.join(inpath,"campaign",campaign,"mom",datestr);
 
 os.chdir(mmclxpath);
 files = [os.path.join(mmclxpath,f) for f in glob.glob('*.mmclx')]
