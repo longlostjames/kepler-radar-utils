09f023df962d211ad46b4b43ba6015f8d698ed76
longlostjames <chris.walden@ncas.ac.uk>
2023-03-01 07:58:42 +0000

    campaign modifications

--- process_kepler.py
+++ process_kepler.py
@@ -1,4 +1,4 @@
-import getopt, sys
+import getopt, sys, os
 
 import datetime
 
@@ -6,7 +6,7 @@ import datetime
 import kepler_utils as kepler
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "d:i:o:", ["date=","inpath=","outpath="])
+    opts, args = getopt.getopt(sys.argv[1:], "d:i:o:c:", ["date=","inpath=","outpath="])
 except getopt.GetoptError as err:
         # print help information and exit:
         print(err) # will print something like "option -a not recognized
@@ -15,15 +15,16 @@ except getopt.GetoptError as err:
 data_date = datetime.datetime.now()
 datestr = data_date.strftime('%Y%m%d')
 
-#inpath = '/Users/cw66/Data/kepler/'
-inpath = '/gws/pw/j07/ncas_obs_vol2/cao/raw_data/ncas-radar-mobile-ka-band-1/data/campaign/picasso-b/mom'
-#outpath = '/Users/cw66/Data/ncas-mobile-radar-ka-band-1/'
-outpath = '/gws/pw/j07/ncas_obs_vol2/cao/processing/ncas-radar-mobile-ka-band-1/picasso-b'
+tracking_tag = 'CFARR_0002';
+# tracking_tag = 'AMOF_20220922221548';
+
+campaign = 'picasso-b';
+
 
 yaml_project_file = '/home/users/cjwalden/git/kepler-radar-utils/amof_projects.yml'
 yaml_instrument_file = '/home/users/cjwalden/git/kepler-radar-utils/amof_radars.yml'
 
-tracking_tag = 'AMOF_20220922221548';
+
 
 
 for o, a in opts:
@@ -33,8 +34,15 @@ for o, a in opts:
         inpath = a;
     elif o == "-o":
         outpath = a;
+    elif o == "-c":
+        campaign = a;
     else:
         assert False, "unhandled option"
+
+#inpath = '/Users/cw66/Data/kepler/'
+inpath = os.path.join('/gws/pw/j07/ncas_obs_vol2/cao/raw_data/ncas-radar-mobile-ka-band-1/data/campaign',campaign,'mom');
+#outpath = '/Users/cw66/Data/ncas-mobile-radar-ka-band-1/'
+outpath = os.path.join('/gws/pw/j07/ncas_obs_vol2/cao/processing/ncas-radar-mobile-ka-band-1',campaign);
 
 kepler.process_kepler(datestr,inpath,outpath,yaml_project_file,yaml_instrument_file,tracking_tag);
 
