a435c2bb2d42a109d3a5c20f768672eecfe405d3
longlostjames <chris.walden@ncas.ac.uk>
2023-11-25 10:44:28 +0000

    Debugging

--- kepler_utils.py
+++ kepler_utils.py
@@ -854,7 +854,7 @@ def convert_kepler_cfradial2l1(infile,outpath,yaml_project_file,yaml_instrument_
 
     scan_type = RadarDataset.scan_type.lower();
 
-    file_timestamp = datetime.datetime.strptime(RadarDataset["time_coverage_start"][:],'%Y-%m-%dT%H:%M:%SZ');
+    file_timestamp = datetime.datetime.strptime(RadarDataset["time_coverage_start"]['data'],'%Y-%m-%dT%H:%M:%SZ');
 
     dtstr = file_timestamp.strftime('%Y%m%d-%H%M%S')
 
@@ -923,7 +923,7 @@ def cfradial_add_ncas_metadata(cfradfile,yaml_project_file,yaml_instrument_file,
 
     scan_type = RadarDataset.scan_type;
 
-    file_timestamp = datetime.datetime.strptime(RadarDataset["time_coverage_start"][:],'%Y-%m-%dT%H:%M:%SZ');
+    file_timestamp = datetime.datetime.strptime(RadarDataset["time_coverage_start"]['data'],'%Y-%m-%dT%H:%M:%SZ');
 
     dtstr = file_timestamp.strftime('%Y%m%d-%H%M%S')
 
