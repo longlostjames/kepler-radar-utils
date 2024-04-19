61669ab0704aead589a64dae17cd159bca737010
longlostjames <chris.walden@ncas.ac.uk>
2024-04-19 16:47:54 +0100

    Debugging

--- kepler_utils.py
+++ kepler_utils.py
@@ -621,7 +621,7 @@ def read_mira35_mmclx(filename, gzip_flag=False, revised_northangle=55.7, **kwar
         azimuth,
         elevation,
         target_scan_rate=target_scan_rate,
-        scan_rate=scan_rate,
+        #scan_rate=scan_rate,
         antenna_transition=antenna_transition,
         instrument_parameters=instrument_parameters,
         radar_calibration=radar_calibration
