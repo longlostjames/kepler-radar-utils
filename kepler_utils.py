b792f2606f737346d3047ccf6e2ae26c74a64b73
longlostjames <chris.walden@ncas.ac.uk>
2024-05-23 11:55:39 +0100

    Debug

--- kepler_utils.py
+++ kepler_utils.py
@@ -1449,7 +1449,7 @@ def find_mmclx_rhi_files(start_time, end_time,azim_min,azim_max,inpath,gzip_flag
                         with nc4.Dataset('dummy', mode='r', memory=gz.read()) as nc:
                             file_time = cftime.num2pydate(nc['time'][0],'seconds since 1970-01-01 00:00:00')
                             #azim = (nc['azi'][0]+nc['northangle'][0]+azimuth_offset) % 360;
-                            azim = (nc['azi'][0]+revised_northangle) % 360;
+                            azim = (nc['azi'][0]+revised_northangle) #% 360;
                             if start_datetime <= file_time <= end_datetime:
                                 print(f'{file_time} {(azim)%360}');
                                 
@@ -1462,11 +1462,11 @@ def find_mmclx_rhi_files(start_time, end_time,azim_min,azim_max,inpath,gzip_flag
                     nc = nc4.Dataset(os.path.join(root, file))
                     file_time = cftime.num2pydate(nc['time'][0],'seconds since 1970-01-01 00:00:00')
                     #azim = (nc['azi'][0]+nc['northangle'][0]+azimuth_offset) % 360;
-                    azim = (nc['azi'][0]+revised_northangle) % 360;
+                    azim = (nc['azi'][0]+revised_northangle) #% 360;
                     if start_datetime <= file_time <= end_datetime:
-                        print(f'{file_time} {(azim)%360}');
+                        print(f'{file_time} {azim}');
 
-                        if azim_min < (azim)%360 <= azim_max:
+                        if azim_min < azim <= azim_max:
                             matching_files.append(os.path.join(root, file))
                     nc.close()         
     return sorted(matching_files)
