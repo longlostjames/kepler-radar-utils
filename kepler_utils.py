9424da14c80e20f44d8c26bb5e69127555b81942
longlostjames <chris.walden@ncas.ac.uk>
2024-05-24 15:13:23 +0100

    debug

--- kepler_utils.py
+++ kepler_utils.py
@@ -582,7 +582,7 @@ def read_mira35_mmclx(filename, gzip_flag=False, revised_northangle=303.15, **kw
 
     radar_calibration = {}
 
-    geometry_correction = {'azimuth_correction': revised_northangle}
+    #geometry_correction = {'azimuth_correction': revised_northangle}
 
 
     #if "PRF-value" in dset.ncattrs():
@@ -634,7 +634,7 @@ def read_mira35_mmclx(filename, gzip_flag=False, revised_northangle=303.15, **kw
         antenna_transition=antenna_transition,
         instrument_parameters=instrument_parameters,
         radar_calibration=radar_calibration,
-        geometry_correction=geometry_correction,
+        #geometry_correction=geometry_correction,
     )
 
     print("last line")
